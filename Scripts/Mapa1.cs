0c6479ee08497f043ffa4b9e66690eabb6a4e4e3
Atenea <avadillog0504@gmail.com>
2024-04-28 13:18:45 +0200

    colisiones

--- Scripts/Mapa1.cs
+++ Scripts/Mapa1.cs
@@ -3,28 +3,51 @@ using Godot;
 public partial class Mapa1 : Node2D
 {
 
-    private TileMap mapa;
-    private int capaTierra = 1;
-    private Jugador jugador;
+	private TileMap mapa;
+	private int capaTierra = 1;
+	private int capaSemillas = 3;
+	private Jugador jugador;
 
-    public override void _Ready()
-    {
-        mapa = GetNode<TileMap>("TileMap");
-        jugador = (Jugador)GetNode<CharacterBody2D>("Jugador");
-    }
+	public override void _Ready()
+	{
+		mapa = GetNode<TileMap>("TileMap");
+		jugador = (Jugador)GetNode<CharacterBody2D>("Jugador");
+	}
 
-    public override void _Process(double delta)
-    {
-        Vector2 characterPosition = jugador.GetCharacterPosition();
+	public override void _Process(double delta)
+	{
+		Vector2I characterPosition = (Vector2I)jugador.GetCharacterPosition();
 
 
-        if (Input.IsActionJustPressed("plow"))
-        {
-            var local = mapa.LocalToMap(characterPosition);
-            GD.Print("Posicion: " + local);
-            var hojaSpritesID = 12;
-            Vector2I coordTierra = new Vector2I(1, 1);
-            mapa.SetCell(capaTierra, local, hojaSpritesID, coordTierra);
-        }
-    }
+		if (Input.IsActionJustPressed("plow"))
+		{
+			Vector2I local = mapa.LocalToMap(characterPosition);
+			int hojaSpritesID = 12;
+			Vector2I coordTierra = new Vector2I(1, 1);
+			mapa.SetCell(capaTierra, local, hojaSpritesID, coordTierra);
+		}
+
+		if (Input.IsActionJustPressed("seeds")) 
+		{
+			Vector2I local = mapa.LocalToMap(characterPosition);
+			TileData tileData = mapa.GetCellTileData(capaTierra, local);
+			Variant semillas = false;
+
+            if (tileData != null)
+			{
+                semillas = tileData.GetCustomData("puede_semillas");
+            }
+			else
+			{
+                GD.Print("No se puede plantar ahi"); //Convertir a popup
+            }
+			int plantasID = 1;
+			Vector2I coordSemilla = new Vector2I(1, 0);
+			
+            if (((bool)semillas) == true)
+            {
+                mapa.SetCell(capaSemillas, local, plantasID, coordSemilla);
+            }
+		}
+	}
 }
--- Scripts/ModoVentana.cs
+++ Scripts/ModoVentana.cs
@@ -2,16 +2,16 @@ using Godot;
 
 public partial class ModoVentana : Control
 {
-    public void _OnFullScreenPressed()
-    {
-        DisplayServer.WindowSetMode(DisplayServer.WindowMode.Fullscreen);
-    }
-    public void _OnMaximizadoPressed()
-    {
-        DisplayServer.WindowSetMode(DisplayServer.WindowMode.Maximized);
-    }
-    public void _OnVentanaPressed()
-    {
-        DisplayServer.WindowSetMode(DisplayServer.WindowMode.Windowed);
-    }
+	public void _OnFullScreenPressed()
+	{
+		DisplayServer.WindowSetMode(DisplayServer.WindowMode.Fullscreen);
+	}
+	public void _OnMaximizadoPressed()
+	{
+		DisplayServer.WindowSetMode(DisplayServer.WindowMode.Maximized);
+	}
+	public void _OnVentanaPressed()
+	{
+		DisplayServer.WindowSetMode(DisplayServer.WindowMode.Windowed);
+	}
 }
